6440df8e7faff5c19498a6c7dace8ec711e263b2
renan <renan95tito@gmail.com>
2026-05-15 20:13:18 -0300

    Implementando Autenticação de Usuários

--- app/urls.py
+++ app/urls.py
@@ -22,7 +22,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('login/', auth_views.LonginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 
     path('', views.home, name='home'),
 
